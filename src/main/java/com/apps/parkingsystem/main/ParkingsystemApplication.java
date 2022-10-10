3dd6746a7447291335dafc9ead831c73f80df67b
albertbisma <50725100+albertbisma@users.noreply.github.com>
2022-10-10 08:34:37 +0700

    Add main class

---
+++ src/main/java/com/apps/parkingsystem/main/ParkingsystemApplication.java
@@ -0,0 +1,27 @@
+package com.apps.parkingsystem.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication(scanBasePackages = {"com.apps.parkingsystem"})
+@EnableAutoConfiguration
+@EnableJpaRepositories("com.apps.parkingsystem.repository")
+@EnableTransactionManagement
+@ComponentScan("com.apps.parkingsystem")
+@EntityScan(basePackages = "com.apps.parkingsystem.entity")
+@EnableSwagger2
+public class ParkingsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingsystemApplication.class, args);
+	}
+	
+
+}
