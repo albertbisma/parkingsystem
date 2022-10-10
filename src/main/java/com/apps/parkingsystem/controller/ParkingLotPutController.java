f644391a8cc097c4e4ffbe847087c5efecd3da6c
albertbisma <50725100+albertbisma@users.noreply.github.com>
2022-10-10 21:00:09 +0700

    Fix URI for Parking Lot Method Put

--- src/main/java/com/apps/parkingsystem/controller/ParkingLotPutController.java
+++ src/main/java/com/apps/parkingsystem/controller/ParkingLotPutController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.apps.parkingsystem.model.GenericResponse;
-import com.apps.parkingsystem.model.ParkingLotRequest;
+import com.apps.parkingsystem.model.ParkingLotPutFilledLotRequest;
 import com.apps.parkingsystem.model.ParkingLotResponse;
 import com.apps.parkingsystem.service.ParkingLotService;
 
@@ -25,13 +25,8 @@ public class ParkingLotPutController {
 	@Autowired
 	private ParkingLotService parkingLotService;
 	
-	@PutMapping("/{id}")
-	public ResponseEntity<ParkingLotResponse> update(@RequestBody ParkingLotRequest request, @PathVariable Integer id){
-		
-		if(request.getAvailableLot() == null) {
-			return ResponseEntity.badRequest().body(generateResponse(HttpStatus.BAD_REQUEST.getReasonPhrase(), "Field availableLot is mandatory."));
-		}
-		
+	@PutMapping("/filledLot/{id}")
+	public ResponseEntity<ParkingLotResponse> update(@RequestBody ParkingLotPutFilledLotRequest request, @PathVariable Integer id){		
 		if (request.getFilledLot() == null) {
 			return ResponseEntity.badRequest().body(generateResponse(HttpStatus.BAD_REQUEST.getReasonPhrase(), "Field filledLot is mandatory."));
 		}
