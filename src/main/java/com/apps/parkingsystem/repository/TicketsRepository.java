c9a06ebbd06585c8e33db4f5e03b9b7963fb64ca
albertbisma <50725100+albertbisma@users.noreply.github.com>
2022-10-10 08:29:59 +0700

    Add Native Query for Get Last ID From table Tickets on database

--- src/main/java/com/apps/parkingsystem/repository/TicketsRepository.java
+++ src/main/java/com/apps/parkingsystem/repository/TicketsRepository.java
@@ -1,6 +1,7 @@
 package com.apps.parkingsystem.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -9,6 +10,7 @@ import com.apps.parkingsystem.entity.Tickets;
 @Transactional
 @Repository
 public interface TicketsRepository extends JpaRepository<Tickets, Integer> {
-	
+	@Query(nativeQuery = true, value = "SELECT MAX(ID) as ID FROM PARKING_MANAGEMENT.TICKETS")
+	Integer getLastIdFromTickets();
 
 }
