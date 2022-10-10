3cd6bdea5702f26700790abe4b6ab52e6f464818
albertbisma <50725100+albertbisma@users.noreply.github.com>
2022-10-10 08:33:33 +0700

    add default unit test

---
+++ src/test/java/com/apps/parkingsystem/ParkingsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apps.parkingsystem;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
