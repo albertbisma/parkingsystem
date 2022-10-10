31ad686be9a37dd58b262cd7404aadfecd8163a4
albertbisma <50725100+albertbisma@users.noreply.github.com>
2022-10-10 21:07:55 +0700

    Add Bean RestTemplate

--- src/main/java/com/apps/parkingsystem/main/ParkingsystemApplication.java
+++ src/main/java/com/apps/parkingsystem/main/ParkingsystemApplication.java
@@ -4,9 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.client.RestTemplate;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -23,5 +26,9 @@ public class ParkingsystemApplication {
 		SpringApplication.run(ParkingsystemApplication.class, args);
 	}
 	
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+	    return builder.build();
+	}
 
 }
